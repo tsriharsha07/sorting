12dce7297b8134b165ec5a8ebfacb6c17264316e
Thonukunuri's <Thonukunuri's@THONUKUNURI>
2021-02-03 16:31:28 +0530

    new sort

---
+++ src/sorting/NewSort.java
@@ -0,0 +1,16 @@
+package sorting;
+import java.util.Arrays;
+public class NewSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = { 64, 34, 25, 12, 22, 11, 90 }; 
+		  
+        Arrays.sort(arr); 
+  
+        System.out.printf("Modified arr[] : %s", 
+                          Arrays.toString(arr));
+
+	}
+
+}
